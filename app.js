2bfe84963222e6c4eae63b031046ee5234838508
Sandvich <24429906+guymelef@users.noreply.github.com>
2023-09-17 15:15:15 +0800

    refactor code for cyclic deployment

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-import mongoose from 'mongoose'
 import express from 'express'
 import cors from 'cors'
 
-import { MONGODB_URI } from './utils/config.js'
-import { fetchAllData } from './utils/card_updater.js'
 import { checkRequestKeyHeader } from './utils/middleware.js'
 import { indexRouter } from './controllers/index.js'
 import { updateRouter } from './controllers/update.js'
@@ -11,14 +8,6 @@ import { updateRouter } from './controllers/update.js'
 
 
 export const app = express()
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(async () => {
-    console.log('Ⓜ️  Connected to MongoDB!')
-    await fetchAllData()
-  })
-  .catch(err => console.log('🟥 MONGODB ERROR:', err.message))
 
 app.use(cors())
 app.use('/', checkRequestKeyHeader, indexRouter)
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-import { app } from "./app.js"
-import { HOST, PORT } from "./utils/config.js"
+import mongoose from "mongoose"
 import http from "http"
 
+import { app } from "./app.js"
+import { fetchAllData } from "./utils/card_updater.js"
+import { MONGODB_URI, HOST, PORT } from "./utils/config.js"
 
 
 const server = http.createServer(app)
-
-server.listen(PORT, () => console.log(`🚀 THE SERVER IS UP AT: ${HOST}${PORT}`))
+mongoose
+  .connect(MONGODB_URI)
+  .then(async () => {
+    console.log('Ⓜ️  Connected to MongoDB!')
+    await fetchAllData()
+    server.listen(PORT, () => console.log(`🚀 THE SERVER IS UP AT: ${HOST}${PORT}`))
+  })
+  .catch(err => console.log('🟥 MONGODB ERROR:', err.message))
